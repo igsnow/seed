47357ed8ea7cfd67faa5665bf2fe647d906127cd
igsnow <zzy716810918@gmail.com>
2020-06-20 19:25:11 +0800

    设置课程机构model

--- apps/organizations/models.py
+++ apps/organizations/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+from apps.users.models import BaseModel
+
+
+# 作为外键
+class City(BaseModel):
+    name = models.CharField(max_length=20, verbose_name='城市')
+    desc = models.CharField(max_length=200, verbose_name='描述')
+
+    class Meta:
+        verbose_name = '城市'
+        verbose_name_plural = verbose_name
+
+
+class CourseOrg(BaseModel):
+    name = models.CharField(max_length=50, verbose_name='机构名称')
+    desc = models.TextField(verbose_name='描述')
+    tag = models.CharField(default='全国知名', max_length=10, verbose_name='机构标签')
+    category = models.CharField(default='pxjg', verbose_name='机构类别', max_length=4,
+                                choices=(('pxjg', '培训机构'), ('gr', '个人'), ('gx', '高校')))
+
+    click_nums = models.IntegerField(default=0, verbose_name='点击数')
+    fav_nums = models.IntegerField(default=0, verbose_name='收藏数')
+    image = models.ImageField(upload_to='org/%Y/%m', verbose_name='logo', max_length=100)
+    address = models.CharField(max_length=150, verbose_name='机构地址')
+    students = models.IntegerField(default=0, verbose_name='学习人数')
+    course_nums = models.IntegerField(default=0, verbose_name='课程数')
+    city = models.ForeignKey(City, on_delete=models.CASCADE, verbose_name='所在城市')
+
+    class Meta:
+        verbose_name = '课程机构'
+        verbose_name_plural = verbose_name

73e80e1a6ac5e7b48903218c5560f88f30ecf676
igsnow <zzy716810918@gmail.com>
2020-06-20 18:12:03 +0800

    设置课程model

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -16,7 +16,18 @@ from apps.users.models import BaseModel
 class Course(BaseModel):
     name = models.CharField(max_length=50, verbose_name="课程名")
     desc = models.CharField(max_length=300, verbose_name="课程描述")
-    
+    learn_times = models.IntegerField(default=0, verbose_name='学习时长(分钟数)')
+    degree = models.CharField(verbose_name='难度', choices=(('cj', '初级'), ('zj', '中级'), ('gj', '高级')), max_length=2)
+    students = models.IntegerField(default=0, verbose_name='学习人数')
+    fav_nums = models.IntegerField(default=0, verbose_name='收藏人数')
+    click_nums = models.IntegerField(default=0, verbose_name='点击数')
+    category = models.CharField(default='后端开发', max_length=20, verbose_name='课程类别')
+    tag = models.CharField(default='', verbose_name='课程标签', max_length=10)
+    youneed_know = models.CharField(default='', max_length=300, verbose_name='课程须知')
+    teacher_tell = models.CharField(default='', max_length=300, verbose_name='老师告诉你')
+    detail = models.TextField(verbose_name='课程详情')
+    image = models.ImageField(upload_to='courses/%Y/%m', verbose_name='封面图', max_length=100)
+
     class Meta:
         verbose_name = "课程信息"
         verbose_name_plural = verbose_name

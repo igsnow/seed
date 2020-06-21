6055561bca74ca15ccc17fcd7349d0125122bde5
igsnow <zzy716810918@gmail.com>
2020-06-21 22:07:40 +0800

    先取出user的model总mobile字段的unique，解决admin创建新用户报错的问题

--- apps/users/models.py
+++ apps/users/models.py
@@ -26,7 +26,7 @@ class UserProfile(AbstractUser):
     birthday = models.DateField(verbose_name='生日', null=True, blank=True)
     gender = models.CharField(max_length=6, verbose_name='性别', choices=GENDER_CHOICE)
     address = models.CharField(max_length=100, verbose_name='地址', default='')
-    mobile = models.CharField(max_length=11, verbose_name='手机号码', unique=True)
+    mobile = models.CharField(max_length=11, verbose_name='手机号码')
     # 配置到media文件夹下面
     image = models.ImageField(verbose_name='用户头像', upload_to='head_image/%Y/%m', default='default.jpg')
 
